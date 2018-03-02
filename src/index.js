6142b983ac031df1208c1abc3335abe5b0763146
blohamen <staver82@gmail.com>
2018-03-02 21:59:12 +0300

    Lol, so easy task)))

--- src/index.js
+++ src/index.js
@@ -1,26 +1,40 @@
 class SmartCalculator {
   constructor(initialValue) {
     // your implementation
+    this.outputValue = initialValue.toString();
   }
 
   add(number) {
     // your implementation
+    this.outputValue+= " + " + number;
+    return this;
   }
   
   subtract(number) {
     // your implementation
+    this.outputValue+= " - " + number;
+    return this;
   }
 
   multiply(number) {
     // your implementation
+    this.outputValue+= " * " + number;
+    return this;
   }
 
   devide(number) {
     // your implementation
+    this.outputValue+= " / " + number;
+    return this;
   }
 
   pow(number) {
     // your implementation
+    this.outputValue+= " ** " + number;
+    return this;
+  }
+  valueOf(){
+    return eval(this.outputValue);
   }
 }
 
